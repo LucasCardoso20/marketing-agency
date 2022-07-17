5746906e2b71d2119fb78236aef25e8249092687
LucasCardoso20 <lucascardosodeoliveira63@gmail.com>
2022-07-17 18:55:42 -0300

    added singlePost content

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,12 +1,50 @@
-import React, {useState} from 'react'
-import {client, urlFor} from '../../client/client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { client, urlFor } from '../../client/client'
+import BlockContent from '@sanity/block-content-to-react'
 
-const SinglePost = ({post, posts}) => {
-  
+const SinglePost = ({ post, posts }) => {
+  const { title, mainImage, body, author, publishedAt, read } = post
+  console.log(post)
   return (
-    <div>
-      <h1>{post.title}</h1>
-    </div>
+    <section>
+      <div className='site-map'>
+        <div className='site-map-container container'>
+          <Link href='/'>
+            <a>
+              <h5>
+                HOME
+              </h5>
+            </a>
+          </Link>
+          <span>|</span>
+          <Link href="/Blog">
+            <a>
+              <h5>BLOG</h5>
+            </a>
+          </Link>
+          <span>|</span>
+          <h5>{title}</h5>
+        </div>
+      </div>
+
+
+      <main className='main-content-single-post'>
+        <article className='blog-post'>
+          <div className='single-post-details'>
+            <h1 className='title-h2'>{title}</h1>
+            
+          </div>
+          <img src={urlFor(mainImage)} />
+
+          <div className='single-post-block-content'>
+            {body[0] !== undefined && (
+              <BlockContent blocks={body} projectId='9luiiryd' dataset='production' />
+            )}
+          </div>
+        </article>
+      </main>
+    </section>
   )
 }
 
@@ -21,7 +59,7 @@ export const getStaticPaths = async () => {
   const posts = await client.fetch(query);
 
   const paths = posts.map((post) => ({
-    params: { 
+    params: {
       slug: post.slug.current
     }
   }));
@@ -32,10 +70,10 @@ export const getStaticPaths = async () => {
   }
 }
 
-export const getStaticProps = async ({ params: { slug }}) => {
+export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "post" && slug.current == '${slug}'][0]`;
   const postsQuery = '*[_type == "post"]'
-  
+
   const post = await client.fetch(query);
   const posts = await client.fetch(postsQuery);
 
